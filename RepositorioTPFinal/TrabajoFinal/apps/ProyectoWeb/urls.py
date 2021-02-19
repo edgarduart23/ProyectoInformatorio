9e241c560a64bfc506437ed9e7f1c1a841c7f520
edgardo <nergumero2017@gmail.com>
2021-02-18 23:02:56 -0300

    aplicacion de plantilla

--- RepositorioTPFinal/TrabajoFinal/apps/ProyectoWeb/urls.py
+++ RepositorioTPFinal/TrabajoFinal/apps/ProyectoWeb/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from apps.ProyectoWeb.views import *
+from . import views
 
 app_name = 'ProyectoWeb'
 urlpatterns = [
        path('noticias/', NoticiasList.as_view()),
+       path('prueba/', views.PruebaIndex)
 ]
 
 
--- RepositorioTPFinal/TrabajoFinal/apps/ProyectoWeb/views.py
+++ RepositorioTPFinal/TrabajoFinal/apps/ProyectoWeb/views.py
@@ -8,3 +8,6 @@ from apps.ProyectoWeb.models import *
 class NoticiasList(ListView):
     model = Noticia
     template_name = 'noticias.html'
+    
+def PruebaIndex(request):
+    return render(request, 'index.html')
