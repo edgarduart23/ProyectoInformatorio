4c59b64afeccbd3ac85986ac04da157c52176181
edgardo <nergumero2017@gmail.com>
2021-02-20 19:48:12 -0300

    agregando imagen al html

--- RepositorioTPFinal/TrabajoFinal/apps/ProyectoWeb/urls.py
+++ RepositorioTPFinal/TrabajoFinal/apps/ProyectoWeb/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from apps.ProyectoWeb.views import *
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 app_name = 'ProyectoWeb'
 urlpatterns = [
        path('noticias/', NoticiasList.as_view()),
        path('prueba/', views.PruebaIndex)
 ]
-
+##urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
