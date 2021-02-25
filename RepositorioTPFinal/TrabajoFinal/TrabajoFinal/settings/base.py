1b57e540180909af99ba7b8ee181fd3a0dfb9327
rioslaboratorio2 <diegoriosarturo@gmail.com>
2021-02-25 11:48:48 -0300

    agregado de aplicacion comentarios

--- RepositorioTPFinal/TrabajoFinal/TrabajoFinal/settings/base.py
+++ RepositorioTPFinal/TrabajoFinal/TrabajoFinal/settings/base.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.ProyectoWeb',
+    'comentarios',
+    
 ]
 
 MIDDLEWARE = [
@@ -127,8 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(os.path.dirname(BASE_DIR),'static')
 ]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
