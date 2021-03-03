44dfb524d58cbc045773332022f1670db4629691
edgardo <nergumero2017@gmail.com>
2021-03-03 15:56:24 -0300

    comment post

---
+++ RepositorioTPFinal/TrabajoFinal/apps/comentarios/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import *
+
+class ComentarioForm(ModelForm):
+    class Meta:
+        model = comentario_comment
+        exclude = ( 'usuario','tiempo_comentario_timestamp')
+        #fields = '__all__'
